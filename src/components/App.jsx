668a84beae18b6bdb2c2ac62579e5bfd9770c7fd
patryq03 <138616976+patryq03@users.noreply.github.com>
2024-01-16 17:54:19 +0100

    feat: change src folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import Section from "./Section/Section";
 import Profile from "./Profile/Profile";
 import Statistics from "./Statistics/Statistics";
-import FriendList from "./FriendList/FriendList";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
+//import FriendList from "./FriendList/FriendList";
+//import TransactionHistory from "./TransactionHistory/TransactionHistory";
 import user from "./data/user.json";
 import uploadStats from "./data/data.json";
-import friends from "./data/friends.json";
-import transactions from "./data/transactions.json";
+//import friends from "./data/friends.json";
+//import transactions from "./data/transactions.json";
 
 export default function App(){
   return (
