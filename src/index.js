3dbfd8509bafdafab778d010235293ea54fd057f
patryq03 <138616976+patryq03@users.noreply.github.com>
2024-01-16 11:49:51 +0100

    feat: change src folder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import App from './components/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDom.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-01-components/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.querySelector('#root')
 );
