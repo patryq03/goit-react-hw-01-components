01d90694bb158e37993d1a61acde017d5402b50c
patryq03 <138616976+patryq03@users.noreply.github.com>
2024-01-17 21:22:40 +0100

    feat: change src folder

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,17 +2,19 @@ import PropTypes from 'prop-types';
 import css from './Statistic.module.css';
 
 export default function Statistics({ title, stats }) {
+  return(
   <div className={css.statistic}>
     <h2 className={css.h2}>{title.toUpperCase()}</h2>
     <ul className={css.list}>
-      {stats.map(stats => (
-        <li key={stats.id} className={css.item}>
-          <span className={css.label}>{stats.label}</span>
-          <span className={css.percentage}>{stats.percentage}</span>
+      {stats.map(({ id, label, percentage }) => (
+        <li key={id} className={css.item}>
+          <span className={css.label}>{label}</span>
+          <span className={css.percentage}>{percentage}</span>
         </li>
       ))}
     </ul>
-  </div>;
+  </div>
+);
 }
 Statistics.propTypes = {
   stats: PropTypes.arrayOf(
